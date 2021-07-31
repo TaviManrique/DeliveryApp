ed244edba66913d32586cc45a953ba55b64d7500
Tavi <tavidanner96@gmail.com>
2021-07-31 02:01:36 -0500

    Se creo el proyecto UsuarioApp

---
+++ app/src/main/java/com/manriquetavi/usuarioapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.manriquetavi.usuarioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
